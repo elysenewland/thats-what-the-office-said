1afb62e24ed5af930259360defa6b4dabd854de7
Elyse Newland <elysenewland@gmail.com>
2023-05-03 11:24:49 -0400

    Add functionality to fetch/display character and quote data dynamically

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,47 @@ import { ref, onMounted } from "vue";
 // Ref objects are mutable and reactive. Can set ref.value to allow values to change
 const quote = ref("");
 const character = ref("");
+const characterColor = ref("");
+const characterImage = ref("");
+const characterIcon = ref("");
+const iconName = ref("");
 const disabled = ref(false);
 const isLoading = ref(false);
 
 //  Get quote and character data from local JSON database asynchronously
 async function getQuote() { 
   try {
+    // Create a loading state and use await new Promise(resolve => setTimeout(resolve, 2000)) to test
     isLoading.value = true;
     character.value = " ";
     quote.value = "Loading ...";
 
-    // Use await new Promise(resolve => setTimeout(resolve, 2000)) to test loading state
+    // Fetch quotes and characters data from their respective local JSON databases
+    const quotesResponse = await fetch("/data/quotesDatabase.json");
+    const charactersResponse = await fetch("/data/charactersDatabase.json");
 
-    const response = await fetch("/database/quotesDatabase.json");
-    const data = await response.json();
+    const quotesData = await quotesResponse.json();
+    const charactersData = await charactersResponse.json();
 
-    // Math.floor() function rounds this number down to the nearest integer to ensure it's a valid number
-    const selectedQuote = Math.floor(Math.random() * data.quotes.length);
-    quote.value = data.quotes[selectedQuote].quote, 
-    character.value = data.quotes[selectedQuote].character;
+    // Randomly choose quote. Define the chosen quote, characterId, then use selectedCharacterId (foreign key) to access the characters database
+    const randomizeQuote = Math.floor(Math.random() * quotesData.quotes.length);
+    const selectedQuote = quotesData.quotes[randomizeQuote];
+    const selectedCharacterId = selectedQuote.characterId;
+    const selectedCharacter = charactersData.characters.find((character) => character.characterId === selectedCharacterId);
+
+    // If the quote and character match, set the values of quote, character, image, and color equal to that selected quote and character. Then set the css variable --character-color to the characterColor and --character-image to characterImage
+    if (selectedQuote && selectedCharacter) {
+      quote.value = selectedQuote.quote;
+      character.value = selectedCharacter.characterName;
+      characterColor.value = selectedCharacter.characterColor;
+      characterImage.value = selectedCharacter.characterImage;
+      characterIcon.value = selectedCharacter.characterIcon;
+      iconName.value = selectedCharacter.iconName;
+      document.documentElement.style.setProperty('--character-color', characterColor.value);
+    }
+    isLoading.value = false;
+
+    // Error handling if quotes aren't available
   } catch(err) {   
       quote.value = "Sorry, we're having trouble finding quotes right now.";
       console.log(err);
@@ -33,12 +55,10 @@ async function getQuote() {
   // Ensure loading state is updated in both success and error cases
   isLoading.value = false;
 }
-
 // Once the component is loaded, call the getQuote function. Allows page to fetch the data from the getQuote function on page refresh
 onMounted(() => {
   getQuote();
 })
-
 // Image spin function. If const disabled is true, cause the image to spin for 600 ms and revert disabled equal to false
 function imgSpin() {
   disabled.value = true;
@@ -50,8 +70,8 @@ function imgSpin() {
 
 <template>
   <main>
-    <!-- Pass character and quote props to Card component -->
-    <Card :subhead="character" :title="quote"></Card>
+    <!-- Pass props to Card component -->
+    <Card :subhead="character" :title="quote" :avatar="characterImage" :icon="characterIcon" :iconName="iconName"></Card>
 
     <!-- Call the img spin class at the button and set it to disabled so that onclick generates the imgSpin function and generates a random quote -->
     <Button :class="{ spin: disabled }" @click="imgSpin(),getQuote()"></Button>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,19 +2,33 @@
 // Export props in order to pass them into Card component
 export default {
     props: {
+        avatar: {
+            type: String, 
+            default: "",
+        },
+        icon: {
+            type: String, 
+            default: "",
+        },
+        iconName: {
+            type: String, 
+            default: "",
+        },
         subhead: String,
         title: String,
-        body: String      
-    }
+        body: String 
+   }
 }
 </script>
 
 <template>
+    <!-- Dynamically pass in the characterColor from app.vue and update the CSS variable -->
     <div class="card">
+        <img v-if="avatar" class="avatar" :src="avatar" :alt="subhead + ' Avatar'" loading="lazy" width="150" height="150" />
         <h1 v-if="subhead">{{ subhead }}</h1>
         <h2 v-if="title">{{ title }}</h2>
         <p v-if="body">{{ body }}</p>
-        <img src="@/assets/images/coffee-icon.svg" alt="Coffee icon">
+        <img v-if="icon" :src="icon" :alt="iconName">
     </div>
 </template>
 
@@ -24,8 +38,17 @@ export default {
         color: var(--white);
         text-align: center;
         border-radius: 1.5rem;
-        padding: 1.5rem 2.125rem;
+        padding: 2.125rem 1.5rem;
+        transition: background-color var(--transition-duration) var(--transition-timing);
     }
+
+    .avatar {
+        margin-bottom: 1.15rem;
+        max-width: 9.375rem;
+        border-radius: 100%;
+        background-color: var(--white-25);
+    }
+
     h1 {
         color: var(--white-80);
         font-size: 0.875rem; 
@@ -34,6 +57,7 @@ export default {
         margin: 0 0 .625rem 0;
         text-transform: uppercase;
     }
+    
     h2 {
         font-size: 1.125rem;
         font-weight: 900;
